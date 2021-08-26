720463c27fe2369a96f31ece579cb77a75d2d555
NachoLacioppa <nacho.lacioppa@gmail.com>
2021-08-26 10:02:46 -0300

    Seccion proyecto

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/*Animacion de Scroll Reveal*/ 
+/*Animacion de Scroll Reveal****/ 
 ScrollReveal().reveal('.navbar', {delay: 500})
 ScrollReveal().reveal('.container', {delay: 500});
 ScrollReveal().reveal('.container-fluid', {delay: 500});
