f4e18a1beb3e0bc05a4470aad0f7de4e8ecc5eb4
NachoLacioppa <nacho.lacioppa@gmail.com>
2021-11-22 12:11:26 -0300

    Actualizacion Noviembre

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 /*Animacion de Scroll Reveal****/ 
+ScrollReveal().reveal('body', {delay: 500})
 ScrollReveal().reveal('.navbar', {delay: 500})
 ScrollReveal().reveal('.container', {delay: 500});
 ScrollReveal().reveal('.container-fluid', {delay: 500});
