5f84950624ffa122372b08e9800c3bab8285479f
NachoLacioppa <nacho.lacioppa@gmail.com>
2022-03-16 15:20:08 -0300

    Update Marzo 2022

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
-ScrollReveal().reveal('.container', {delay: 250});
-ScrollReveal().reveal('.container-fluid', {delay: 250});
+/*Animacion de Scroll Reveal****/ 
+ScrollReveal().reveal('body', {delay: 500})
+ScrollReveal().reveal('.navbar', {delay: 500})
+ScrollReveal().reveal('.container', {delay: 500});
+ScrollReveal().reveal('.container-fluid', {delay: 500});
